6504634d7248cbd9b1574cf34aa4c49dda38acee
Joao-Vitor17 <joaovitorsantosdiada123@gmail.com>
2026-03-11 10:58:09 -0300

    feat: configuração inicial de uma API básica com Node.js e Jest

---
+++ src/services/livroService.js
@@ -0,0 +1,11 @@
+const livros = [];
+
+const criarLivro = (titulo, autor) => {
+    const livro = { id: livros.length + 1, titulo, autor };
+    livros.push(livro);
+    return livro;
+}
+
+module.exports = {
+    criarLivro
+}
